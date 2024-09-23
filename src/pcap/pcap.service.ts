66249a63ed802d0ee33c697714fe423a45239b49
smw0807 <smw0807@gmail.com>
2024-09-23 23:00:22 +0900

    패킷 추출

--- src/pcap/pcap.service.ts
+++ src/pcap/pcap.service.ts
@@ -1,46 +1,21 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import * as pcap from 'pcap';
+import { PcapSession } from 'pcap';
 
 @Injectable()
 export class PcapService implements OnModuleInit {
   private tcp_tracker = new pcap.TCPTracker();
-  private pcap_session = null;
+  private pcap_session: PcapSession = null;
   onModuleInit() {
     console.log('PcapModule has been initialized.');
     this.startCapture();
   }
 
   startCapture() {
-    // const pcapSession = pcap.createSession('', 'tcp');
-
-    // pcapSession.on('packet', (rawPacket) => {
-    //   const packet = pcap.decode.packet(rawPacket);
-    //   console.log(packet);
-    // });
-    this.pcap_session = pcap.createSession('', {
-      filter: 'ip proto \\tcp',
-    });
-
-    this.tcp_tracker.on('session', function (session) {
-      console.log(
-        'Start of session between ' +
-          session.src_name +
-          ' and ' +
-          session.dst_name,
-      );
-      session.on('end', function (session) {
-        console.log(
-          'End of TCP session between ' +
-            session.src_name +
-            ' and ' +
-            session.dst_name,
-        );
-      });
-    });
-
-    this.pcap_session.on('packet', function (raw_packet) {
+    this.pcap_session = pcap.createSession('en0');
+    this.pcap_session.on('packet', (raw_packet) => {
       const packet = pcap.decode.packet(raw_packet);
-      this.tcp_tracker.track_packet(packet);
+      console.dir(packet, { depth: null });
     });
   }
 }
