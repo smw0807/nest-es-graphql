497c8004d7f180b3edc09c0d3e4482a4a5f6040a
smw0807 <smw0807@gmail.com>
2024-09-27 14:14:22 +0900

    consumer일 경우에만 엘라스틱서치에 연결하도록 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,13 @@ import { GraphqlModule } from './graphql/graphql.module';
 import { PcapModule } from './pcap/pcap.module';
 
 @Module({
-  imports: [ConfigModule, ElasticSearchModule, GraphqlModule, PcapModule],
+  imports: [
+    ConfigModule,
+    // APP_RULE이 consumer일 경우에만 ElasticSearchModule을 import
+    ...(process.env.APP_ROLE === 'consumer' ? [ElasticSearchModule] : []),
+    GraphqlModule,
+    PcapModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
